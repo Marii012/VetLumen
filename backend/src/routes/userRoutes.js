ebef5e54e730260a0efc5d432cedc4b87e65dff7
Mariana Fernandes <pv30216@alunos.estgv.ipv.pt>
2026-07-18 16:22:59 +0100

    update

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -8,8 +8,6 @@ router.get('/:id', userController.getUserById);
 
 router.put('/:id', userController.updateUser);
 
-// rota de reset-password removida: funcionalidade retirada do painel Admin
-
 router.delete('/:id', userController.deleteUser);
 
 module.exports = router;
