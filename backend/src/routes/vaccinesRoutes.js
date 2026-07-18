7d17978de6fcf7478c8631fb06f1d1c23da9e08d
Mariana Fernandes <pv30216@alunos.estgv.ipv.pt>
2026-07-18 16:27:17 +0100

    update route

--- backend/src/routes/vaccinesRoutes.js
+++ backend/src/routes/vaccinesRoutes.js
@@ -4,27 +4,16 @@ const router = express.Router();
 const vaccineController = require('../controllers/vaccinesController');
 
 
-// LISTAR TODAS AS VACINAS
 router.get('/', vaccineController.getAllVaccines);
 
-
-// LISTAR VACINAS POR PET
 router.get('/pet/:id', vaccineController.getVaccinesByPet);
 
-
-// OBTER VACINA POR ID
 router.get('/:id', vaccineController.getVaccineById);
 
-
-// CRIAR VACINA
 router.post('/', vaccineController.createVaccine);
 
-
-// ATUALIZAR VACINA
 router.put('/:id', vaccineController.updateVaccine);
 
-
-// ELIMINAR VACINA
 router.delete('/:id', vaccineController.deleteVaccine);
 
 
